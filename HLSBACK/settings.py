ff32563f01bf43e4b5a5895fb8e357218c6bf3d4
Pharm Shegz <shegzworld@gmail.com>
2024-11-02 15:05:00 +0100

    reconfigure whitenoise in settings

--- HLSBACK/settings.py
+++ HLSBACK/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_URL = 'media/'
