fdd872350ce006e1541072cd5f57d422f3994782
Pharm Shegz <shegzworld@gmail.com>
2024-11-03 07:31:26 -1100

    still on the lifestylerating

--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('NT_gallery', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
