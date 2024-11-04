b1c838de99b977c040618a5c842d3cb12536a00f
Pharm Shegz <shegzworld@gmail.com>
2024-11-04 04:40:25 -1100

    still on the lifestylerating

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     
