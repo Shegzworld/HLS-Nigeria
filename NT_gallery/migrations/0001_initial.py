ee7b790e57fd3f90e8123ccaeb0943d0b9991e89
Pharm Shegz <shegzworld@gmail.com>
2024-11-04 01:39:30 -1100

    still on the lifestylerating

--- NT_gallery/migrations/0001_initial.py
+++ NT_gallery/migrations/0001_initial.py
@@ -47,7 +47,7 @@ class Migration(migrations.Migration):
                 ('category', models.CharField(max_length=255)),
                 ('benefits', models.TextField()),
                 ('price', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('addresses', models.ManyToManyField(to='NT_gallery.healthgoal')),
+                # ('addresses', models.ManyToManyField(to='NT_gallery.healthgoal')),
                 ('lifestyle_compatibility', models.ManyToManyField(to='NT_gallery.lifestyle')),
                 ('suitable_for', models.ManyToManyField(to='NT_gallery.medicalcondition')),
             ],
