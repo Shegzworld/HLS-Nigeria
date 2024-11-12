c0e755da74db640694e10e6716c7ce0334510ad9
Folexy13 <folajimiopeyemisax13@gmail.com>
2024-11-12 05:44:15 -0800

    ...

--- HLSBACK/settings.py
+++ HLSBACK/settings.py
@@ -132,9 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-    {
-        # 'NAME': 'HLSBACK.validators.CustomPasswordValidator',
-    },
+    
 ]
 
 
