4340a638d74a768108b3209a1e1d781f6044981d
Folexy13 <folajimiopeyemisax13@gmail.com>
2024-11-12 12:23:47 -0800

    ...

--- HLSBACK/settings.py
+++ HLSBACK/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['hlsnigeria-e0c4b5df87f5.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['hlsnigeria-e0c4b5df87f5.herokuapp.com','127.0.0.1:8000','https://hls-vr1z.onrender.com']
 # ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
