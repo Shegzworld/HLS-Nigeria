5eb1538aa7d895b5b57de7114e88fe5ff07f1eb2
Folexy13 <folajimiopeyemisax13@gmail.com>
2024-11-13 16:29:01 -0800

    statics file loading --- 2

--- HLSBACK/settings.py
+++ HLSBACK/settings.py
@@ -108,42 +108,42 @@ WSGI_APPLICATION = 'HLSBACK.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DEBUG = False
+DEBUG = True
 
-if DEBUG:
-        DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR/'db.sqlite3',
-        }
-    }
-else: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            # 'NAME': 'dbuap6sb6d2e99',
-            # 'USER': 'udrt2vnk83cb27',
-            # 'PASSWORD': 'p695d0b8f20d05a6899f36c244b7262ec9fc22d8417d98e5fa61a31a70ac1f80b',
-            # 'HOST': 'c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-            # 'PORT': '5432',
-            # 'OPTIONS': {
-            #     'autocommit': True,
-            #     'sslmode': 'require',
-            # }
-        }
-    }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dbuap6sb6d2e99',
-#         'User': 'udrt2vnk83cb27',
-#         'PASSWORD': 'p695d0b8f20d05a6899f36c244b7262ec9fc22d8417d98e5fa61a31a70ac1f80b',
-#         'HOST': 'c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432'
+# if DEBUG:
+#         DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR/'db.sqlite3',
+#         }
 #     }
-# }
+# else: 
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             # 'NAME': 'dbuap6sb6d2e99',
+#             # 'USER': 'udrt2vnk83cb27',
+#             # 'PASSWORD': 'p695d0b8f20d05a6899f36c244b7262ec9fc22d8417d98e5fa61a31a70ac1f80b',
+#             # 'HOST': 'c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+#             # 'PORT': '5432',
+#             # 'OPTIONS': {
+#             #     'autocommit': True,
+#             #     'sslmode': 'require',
+#             # }
+#         }
+#     }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'dbuap6sb6d2e99',
+        # 'User': 'udrt2vnk83cb27',
+        # 'PASSWORD': 'p695d0b8f20d05a6899f36c244b7262ec9fc22d8417d98e5fa61a31a70ac1f80b',
+        # 'HOST': 'c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+        # 'PORT': '5432'
+    }
+}
 
 
 # Password validation
