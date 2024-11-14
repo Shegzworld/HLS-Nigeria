6f4a7dda9bcfb4b7993489e622fd7c7e056af5a1
Folexy13 <folajimiopeyemisax13@gmail.com>
2024-11-13 16:36:06 -0800

    statics file loading --- 2

--- NT_gallery/admin.py
+++ NT_gallery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Health_Benefits,Health_support,Fortify,Side_effects
+from .models import Product, Health_Benefits, Health_support, Fortify, Side_effects
 
 # Define admin interface for Product model
 
@@ -13,10 +13,14 @@ class ProductAdmin(admin.ModelAdmin):
     inlines = [HealthSupportInline]
 
     def get_health_conditions(self, obj):
-        conditions = [hs.health_condition for hs in obj.health_support_set.all()]
-        return ", ".join(conditions)
+        # Check if health_support is associated with this product
+        if obj.health_support:
+            return obj.health_support.health_condition
+        return "No health support assigned"
+
+    get_health_conditions.short_description = "Health Conditions"  # Optional: Set column header in admin display
 
 # Register other models with default admin interface
-models = [ Health_support, Fortify, Side_effects, Health_Benefits]
+models = [Health_support, Fortify, Side_effects, Health_Benefits]
 for model in models:
     admin.site.register(model)
