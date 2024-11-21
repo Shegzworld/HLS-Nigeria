e4f89606128ac85fec8bf97584b3242e614f9f99
Shegzworld <104302152+Shegzworld@users.noreply.github.com>
2024-11-21 03:40:09 -1100

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,17 @@ class BlogAdmin(admin.ModelAdmin):
     readonly_fields = ('slug',)  # Make the slug field read-only
     list_display = ('title', 'slug', 'created_at', 'updated_at', 'author') 
 
+class BlogAdminArea(admin.AdminSite):
+    site_header = 'HLS Blog Portal'
+
+blog_site = BlogAdminArea(name = 'Blog portal')
+
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(Author)
 admin.site.register(Comment)
 admin.site.register(Category)
+
+blog_site.register(Blog)
+blog_site.register(Author)
+blog_site.register(Comment)
+blog_site.register(Category)
