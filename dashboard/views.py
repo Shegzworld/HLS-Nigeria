38673b5adb76102422c8868543123721897a627d
Aluko Opeyemi Folajimi <62119915+Folexy13@users.noreply.github.com>
2024-11-26 14:43:28 +0100

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -71,7 +71,7 @@ class Dashboard(LoginRequiredMixin, TemplateView):
         user_lifestyle = Lifestyle.objects.filter(
             user_profile=user_profile)
         products_with_health_benefits = Product.objects.filter(
-                # Q(health_benefit__fortify__in=[some_fortify_value]) |  
+                Q(health_benefit__fortify__in=user_lifestyle) |  
                 #  Q(health_benefit__health_support__health_condition__in=user_health)
          ).select_related('health_benefit').prefetch_related('health_benefit__fortify', 'health_benefit__health_support')
         
