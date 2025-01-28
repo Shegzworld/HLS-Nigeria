9f7d3ab33f1087ebb358e694f57ad771da274afb
Aluko Opeyemi Folajimi <62119915+Folexy13@users.noreply.github.com>
2025-01-28 23:35:01 +0100

    Update settings.py

--- HLSBACK/settings.py
+++ HLSBACK/settings.py
@@ -90,6 +90,17 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
     'widget_tweaks',
 ]
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
