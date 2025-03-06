cc99ca9d1cb3a5fbd03754dc47be54c7b6cdb1e7
Folexy13 <folajimiopeyemisax13@gmail.com>
2025-03-06 14:03:42 +0100

    ...

--- api/views.py
+++ api/views.py
@@ -276,6 +276,18 @@ class SupplementViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ArticleViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
